5bbc8375aa33f98d626043786e043da2380dad82
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-08 19:51:10 +0100

    Add homepage to author show page

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -15,5 +15,6 @@ describe "Show author page", type: :feature do
     # these are the standard names given to inputs by the form builder
     expect(page).to have_text('Alan')
     expect(page).to have_text('Turing')
+    expect(page).to have_text('http://wikipedia.de/Alan_Turing')
   end
 end
