243e70091c25fe951d32f159ffa5542b0b518407
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-08 21:23:05 +0100

    Add failing tests for #11

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  it "should not save an author without last_name" do
+  it "should not save an author without title" do
     new_author = Paper.create(venue: 'NYC', year: 1991)
+    expect(new_author).to_not be_valid
+  end
+
+  it "should not save an author without venue" do
+    new_author = Paper.create(title: 'A Paper', year: 1991)
+    expect(new_author).to_not be_valid
+  end
+
+  it "should not save an author without year" do
+    new_author = Paper.create(title: 'A Paper', venue: 'NYC')
     expect(new_author).to_not be_valid
   end
 end
