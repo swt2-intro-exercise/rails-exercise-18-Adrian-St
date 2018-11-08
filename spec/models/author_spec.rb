d5e2fc7ffb40c79822cef69d619da7f7454ab4a1
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-08 19:21:55 +0100

    Failing test for #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+  let(:author) { FactoryBot.create(:author) }
+
   it "creates a new author" do
-    author = Author.create(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
-    expect(author.first_name).to eq('Alan')
-    expect(author.last_name).to eq('Turing')
-    expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
-    expect(Author.last).to eq(author)
+    new_author = Author.create(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+    expect(new_author.first_name).to eq('Alan')
+    expect(new_author.last_name).to eq('Turing')
+    expect(new_author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+    expect(Author.last).to eq(new_author)
+  end
+
+  it "returns the full name for author.name" do
+    expect(author.name).to eq('Alan Turing')
   end
 end
