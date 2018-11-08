4525a0756bd6a59688051ebbb9f3a9ffc7b9beff
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-08 21:39:15 +0100

    Create failing test for #13

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,22 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   it "should not save a paper without title" do
-    new_author = Paper.create(venue: 'NYC', year: 1991)
-    expect(new_author).to_not be_valid
+    new_paper = Paper.create(venue: 'NYC', year: 1991)
+    expect(new_paper).to_not be_valid
   end
 
   it "should not save a paper without venue" do
-    new_author = Paper.create(title: 'A Paper', year: 1991)
-    expect(new_author).to_not be_valid
+    new_paper = Paper.create(title: 'A Paper', year: 1991)
+    expect(new_paper).to_not be_valid
   end
 
   it "should not save a paper without year" do
-    new_author = Paper.create(title: 'A Paper', venue: 'NYC')
-    expect(new_author).to_not be_valid
+    new_paper = Paper.create(title: 'A Paper', venue: 'NYC')
+    expect(new_paper).to_not be_valid
   end
 
   it "should not save a paper with a year as string" do
-    new_author = Paper.create(title: 'A Paper', venue: 'NYC', year: 'nineteen-fifty')
-    expect(new_author).to_not be_valid
+    new_paper = Paper.create(title: 'A Paper', venue: 'NYC', year: 'nineteen-fifty')
+    expect(new_paper).to_not be_valid
   end
+
+  it "should have an empty list of authors" do
+    new_paper = FactoryBot.create(:paper)
+    expect(new_paper.authors).to eql([])
+  end
+
 end
