b157d0eeeec871d7030364f9c34abc0e758ad9f4
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-08 19:10:56 +0100

    Failing test for #3

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  author = Author.create(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+  expect(author.first_name).to eq('Alan')
+  expect(author.last_name).to eq('Turing')
+  expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+  expect(Author.last).to eq(author)
 end
