777a93c7aa2e1d816a98db7c15b8e467c55a3b43
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-08 21:29:34 +0100

    Add failint test for #12

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  it "should not save an author without title" do
+  it "should not save a paper without title" do
     new_author = Paper.create(venue: 'NYC', year: 1991)
     expect(new_author).to_not be_valid
   end
 
-  it "should not save an author without venue" do
+  it "should not save a paper without venue" do
     new_author = Paper.create(title: 'A Paper', year: 1991)
     expect(new_author).to_not be_valid
   end
 
-  it "should not save an author without year" do
+  it "should not save a paper without year" do
     new_author = Paper.create(title: 'A Paper', venue: 'NYC')
+    expect(new_author).to_not be_valid
+  end
+
+  it "should not save a paper with a year as string" do
+    new_author = Paper.create(title: 'A Paper', venue: 'NYC', year: 'nineteen-fifty')
     expect(new_author).to_not be_valid
   end
 end
