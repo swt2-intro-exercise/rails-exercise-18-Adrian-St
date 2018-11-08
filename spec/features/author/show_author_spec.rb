b9c8a6bad5426b10a195cdcc44c35a11d8dcba44
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-08 19:32:01 +0100

    Failing test for #5

---
+++ spec/features/author/show_author_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Show author page", type: :feature do
+  before do
+    @alan = FactoryBot.create(:author)
+  end
+
+  it "should render withour error" do
+    visit author_path(@alan)
+  end
+
+  it "should have text inputs for an author's first name, last name, and homepage" do
+    visit author_path(@alan)
+
+    # these are the standard names given to inputs by the form builder
+    expect(page).to have_text('Alan')
+  end
+end
