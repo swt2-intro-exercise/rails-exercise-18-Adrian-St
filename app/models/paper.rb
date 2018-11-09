c353eb3083c8c15d29d463aec9591843b1d63eb0
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-09 10:41:59 +0100

    Add year filter to paper index #17#

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -4,4 +4,6 @@ class Paper < ApplicationRecord
   validates :title, presence: true
   validates :venue, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
+
+  scope :year, ->(year) { year ? where(year: year) : nil}
 end
